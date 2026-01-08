8b297e2bec6eee46532916780379e753017f9db7
Lakshay Gambhir <lakshaygambhir96@gmail.com>
2026-01-08 11:54:30 +0530

    tailwind basics

---
+++ 03_tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-2xl'>Tailwind test</h1>
+    <Card username = "Lakshay" btnText="dont visit me"/>
+    </>
+  )
+}
+
+export default App
